78c368ac27f925cb350a85838fb91e2228a6a013
Jason John <jkj1982@gmail.com>
2017-01-02 23:03:16 -0500

    updates

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBMzqsWHQiernaxaAgL_yCvmGD36F8Alb8",
+  authDomain: "catch-of-the-day-jason-john.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-jason-john.firebaseio.com",
+});
+
+export default base;
